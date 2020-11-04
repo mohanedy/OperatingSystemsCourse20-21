f135e933f7e2c387222b26014de2dc869fa5515f
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2020-11-04 15:05:44 +0200

    Task1Starter Project Added

---
+++ Task1StarterProject/src/com/task1/Task1.java
@@ -0,0 +1,64 @@
+package com.task1;
+
+/*
+TODO Task Hint:
+    copy this line to be able to define Scanner object to Take input from user
+    Scanner input = new Scanner(System.in);
+*/
+public class Task1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+        TODO: Question 1: (Sum the digits in an integer) Write a method that computes the sum of the digits in an long.
+                * Use the following method header:
+                public static long sumDigits(long n)
+                ---------------------------------------------
+                * Explanation:
+                For example, sumDigits(234) returns 9 (= 2 + 3 + 4).
+                (Hint: Use the % operator to extract digits and the / operator to remove the extracted digit. For instance,
+                to extract 4 from 234, use 234 % 10 (= 4 ). To remove 4 from 234, use 234 / 10
+                (= 2 3 ). Use a loop to repeatedly extract and remove the digit until all the digits
+                are extracted.
+                 > Bonus : Learn more about Math.abs() method and try to use it in the task through it's documentation
+                ----------------------------------------------
+                * Write a method that prompts the user to enter an long then displays the sum of all its digits and call
+                 this method in main
+                method to test the program.
+
+    */
+
+    //TODO Write Answer for Question 1 Here
+
+
+    //-----------------------------------------------------------------------//
+
+    /*
+        TODO: Question 2: (Sort three numbers) Write a method with the following header to display three
+                            numbers in decreasing order:
+                            ---------------------
+                            public static void displaySortedNumbers(double num1, double num2, double num3)
+                            ---------------------
+                            Write a test program that prompts the user to enter three numbers and invokes the
+                            method to display them in decreasing order.
+    */
+
+    //TODO Write Answer for Question 2 Here
+
+
+    //-----------------------------------------------------------------------//
+
+    /*
+    TODO: Question 3: Write a method that prompts a student to enter a score.
+          If the score is greater or equal to 60, display “you pass the exam”;
+          otherwise, display “you don’t pass the exam”. Your program ends with input -1.
+    */
+
+    //TODO Write Answer for Question 3 Here
+
+
+    //-----------------------------------------------------------------------//
+
+}
