eceb8149c79e5141056c85111736be1448510b11
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2020-12-22 17:25:44 +0200

    Added Task 3

---
+++ Task3StarterProject/src/Task3.java
@@ -0,0 +1,75 @@
+
+/*
+TODO Task Hint:
+        copy this line to be able to define Scanner object that will be used to take input from user
+        Scanner input = new Scanner(System.in);
+*/
+public class Task3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+        TODO: Question 1: (Assign grades) Write a program that reads student scores, gets the best score, and
+                                    then assigns grades based on the following scheme:
+                                    Grade is A if score is >=  bestScore - 5;
+                                    Grade is B if score is >=  bestScore - 10;
+                                    Grade is C if score is >=  bestScore - 15;
+                                    Grade is D if score is >=  bestScore - 20;
+                                    Grade is F otherwise.
+                                    The program prompts the user to enter the total number of students, and then
+                                    prompts the user to enter all of the scores, and concludes by displaying the grades.
+                                    ----------------------------------------------------------------------------------
+                                    Here is a sample run:
+                                    ----------------------------------------------------------------------------------
+                                    Enter the number of students: 4
+                                    Enter 4 scores: 40 55 70 58
+                                    Student 0 score is 40 and grade is F
+                                    Student 1 score is 55 and grade is C
+                                    Student 2 score is 70 and grade is A
+                                    Student 3 score is 58 and grade is C
+                                    .
+    */
+
+    //TODO Write Answer for Question 1 Here
+
+
+    //-----------------------------------------------------------------------//
+
+    /*
+                TODO: Question 2: Write a program that reads ten integers, and then display the number of even numbers and odd numbers.
+                                  Assume that the input ends with 0.
+                                  ----------------------------------------------------------------------------------
+                                  Here is the sample run of the program.
+                                  ----------------------------------------------------------------------------------
+                                  Enter numbers: 1 2 3 2 1 6 3 4 5 2 3 6 8 9 9 0
+                                  The number of odd numbers: 8
+                                  The number of even numbers: 7
+    */
+
+    //TODO Write Answer for Question 2 Here
+
+    //-----------------------------------------------------------------------//
+
+    /*
+    TODO: Question 3:   design a class named Rectangle to represent a rectangle. The class contains:
+                        --------------------------------------------------------------------------------
+                        ■■ Two double data fields named width and height that specify the width and
+                        height of the rectangle. The default values are 1 for both width and height.
+                        ■■ A no-arg constructor that creates a default rectangle.
+                        ■■ A constructor that creates a rectangle with the specified width and height.
+                        ■■ A method named getArea() that returns the area of this rectangle.
+                        ■■ A method named getPerimeter() that returns the perimeter.
+                        --------------------------------------------------------------------------------
+                        Write a test program that creates two Rectangle objects—one with width 4 and height 40, and
+                        the other with width 3.5 and height 35.9. Display the width, height, area, and
+                        perimeter of each rectangle in this order
+    */
+
+    //TODO Write Answer for Question 3 Here and define the class outside this class but in the same file:
+
+
+    //-----------------------------------------------------------------------//
+
+}
